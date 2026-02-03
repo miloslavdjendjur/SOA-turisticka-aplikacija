1cda885373e9be0d283c10460410486d15c55474
Miloslav Djendjur <djendjur.m@gmail.com>
2026-02-03 13:48:44 +0100

    gateway

---
+++ gateway/go.mod
@@ -0,0 +1,7 @@
+module gateway
+
+go 1.21
+
+require (
+	github.com/gin-gonic/gin v1.9.1
+)
---
+++ gateway/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	stakeholdersTarget := "http://stakeholders:8081"
+	toursTarget := "http://tours:8082"
+	followersTarget := "http://followers:8083"
+
+	// Rute za Stakeholders (Java)
+	r.Any("/api/users/*path", reverseProxy(stakeholdersTarget))
+
+	// Rute za Tours (Go)
+	r.Any("/tours", reverseProxy(toursTarget))
+	r.Any("/keypoints", reverseProxy(toursTarget))
+	r.Any("/blogs", reverseProxy(toursTarget))
+
+	// Rute za Followers (Go + Neo4j)
+	r.Any("/followers", reverseProxy(followersTarget))
+
+	log.Println("Gateway running on port 8000")
+	r.Run(":8000")
+}
+
+// Pomocna funkcija koja pravi Reverse Proxy
+func reverseProxy(target string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		remote, err := url.Parse(target)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Invalid proxy target"})
+			return
+		}
+
+		proxy := httputil.NewSingleHostReverseProxy(remote)
+		proxy.Director = func(req *http.Request) {
+			req.Header = c.Request.Header
+			req.Host = remote.Host
+			req.URL.Scheme = remote.Scheme
+			req.URL.Host = remote.Host
+			req.URL.Path = c.Request.URL.Path
+		}
+
+		proxy.ServeHTTP(c.Writer, c.Request)
+	}
+}
